35478a0578943c8f8ec8a6067b504d0847d23901
penguo <32843456+penguo@users.noreply.github.com>
2017-11-06 13:22:31 +0900

    171106 검색기능까지 구현
    v7.Appcompat 타입 적용되는지 확인해봐야함.

--- app/src/main/java/com/pepg/easysearchwidget/MainActivity.java
+++ app/src/main/java/com/pepg/easysearchwidget/MainActivity.java
@@ -3,8 +3,10 @@ package com.pepg.easysearchwidget;
 import android.app.Activity;
 import android.content.Context;
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.graphics.drawable.ColorDrawable;
 import android.app.AlertDialog;
+import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -12,6 +14,7 @@ import android.view.View;
 import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
@@ -21,6 +24,7 @@ public class MainActivity extends Activity {
     Context context;
     private View decorView;
     private int uiOption;
+    public String inputString;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -59,13 +63,25 @@ public class MainActivity extends Activity {
         AlertDialog.Builder builder = new AlertDialog.Builder(context);
         AlertDialog dialog;
 
-        final EditText et = dialogLayout.findViewById(R.id.dialog_et);
         final TextView tv = dialogLayout.findViewById(R.id.dialog_tv);
-        final Button btnSearch = dialogLayout.findViewById(R.id.dialog_btn_search);
+        final EditText et = dialogLayout.findViewById(R.id.dialog_et);
+        final ImageButton btnSearch = dialogLayout.findViewById(R.id.dialog_btn_search);
+
+        tv.setText("NAVER 영어사전");
 
         builder.setView(dialogLayout);
 
-        dialog = builder.create(); //builder.show()를 create하여 dialog에 저장하는 방식.
+        btnSearch.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                inputString = et.getText().toString();
+                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://endic.naver.com/" + inputString));
+                startActivity(intent);
+                finish();
+            }
+        });
+
+        dialog = builder.create();
         dialog.getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_VISIBLE);
         dialog.show();
     }
